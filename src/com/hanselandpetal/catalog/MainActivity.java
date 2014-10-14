bff258e57c707ad727b2192ea3f766d25e12dc3b
Simran5590 <Simran5590@gmail.com>
2014-10-14 11:29:42 -0700

    Serial Tasks

---
+++ src/com/hanselandpetal/catalog/MainActivity.java
@@ -0,0 +1,106 @@
+package com.hanselandpetal.catalog;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends Activity
+{
+	ProgressBar progressBar;
+	TextView	output;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// Initialize the TextView for vertical scrolling
+		output = (TextView) findViewById(R.id.textView);
+		output.setMovementMethod(new ScrollingMovementMethod());
+		
+		progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+		progressBar.setVisibility(View.INVISIBLE);
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		if (item.getItemId() == R.id.action_do_task)
+		{
+			MyTask task = new MyTask();
+			task.execute("Param 1", "Param 2", "Param 3");
+		}
+		return false;
+	}
+	
+	protected void updateDisplay(String message)
+	{
+		output.append(message + "\n");
+	}
+	
+	public class MyTask extends AsyncTask<String, String, String>
+	{
+		
+		@Override
+		protected void onPreExecute()
+		{
+			progressBar.setVisibility(View.VISIBLE);
+			updateDisplay("Starting Task");
+		}
+
+		@Override
+		protected String doInBackground(String... params)
+		{
+			for(int i=0; i< params.length; i++)
+			{
+				
+				try
+				{
+					
+					// It is used to update UI.
+					publishProgress("Working with: " + params[i]);
+					Thread.sleep(1000);
+				}
+				catch (InterruptedException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return "Finishing Task";
+		}
+		
+		@Override
+		protected void onProgressUpdate(String... values)
+		{
+			updateDisplay(values[0]);
+			
+		}
+		
+		
+		
+		
+		@Override
+		protected void onPostExecute(String result)
+		{
+			updateDisplay(result);
+			progressBar.setVisibility(View.INVISIBLE);
+		}
+	}
+	
+}
